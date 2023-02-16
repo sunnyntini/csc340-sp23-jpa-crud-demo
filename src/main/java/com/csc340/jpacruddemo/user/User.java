fb26de89f84dec822e1c88c241cf16b54a4d1069
sunnyntini <100369553+sunnyntini@users.noreply.github.com>
2023-02-16 09:04:22 -0500

    Update User.java

--- src/main/java/com/csc340/jpacruddemo/user/User.java
+++ src/main/java/com/csc340/jpacruddemo/user/User.java
@@ -23,7 +23,7 @@ import lombok.Setter;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String userName;
     private String email;
